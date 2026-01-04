7a305e6a0be10c00846744e6ae01463fc5945b5f
elvaivas <jvivasgabriel@gmail.com>
2026-01-04 13:50:07 -0400

    comentar from webdriver_manager.chrome import ChromeDriverManager~ si funciona borrar correccion 2

--- tasks/scraper/order_scraper.py
+++ tasks/scraper/order_scraper.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service as ChromeService
-#from webdriver_manager.chrome import ChromeDriverManager
 from app.core.config import settings
 
 logging.basicConfig(level=logging.INFO)
@@ -23,6 +22,7 @@ class OrderScraper:
 
     def setup_driver(self):
         if self.driver: return
+        logger.info("[Scraper] Iniciando Driver (Nativo)...")
         chrome_options = Options()
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
@@ -30,7 +30,11 @@ class OrderScraper:
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--remote-allow-origins=*")
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-        service = ChromeService(ChromeDriverManager().install())
+        
+        # --- CORRECCIÓN AQUÍ: Sin argumentos ---
+        service = ChromeService() 
+        # ---------------------------------------
+        
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
     def login(self) -> bool:
